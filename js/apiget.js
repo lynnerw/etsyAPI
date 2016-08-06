223287dd15e11a8733b6f65a7cd5edc5e12cff97
lynnerw <larwitter@gmail.com>
2016-08-05 20:40:01 -0400

    renamed some of the more-less vars for troubleshooting

--- js/apiget.js
+++ js/apiget.js
@@ -44,35 +44,38 @@
       var moretext = "More";
       var lesstext = "Less";
 
-      $('.more').each(function() {
-          var content = $(this).html();
+       $('.more').each(function() {
+           var content = $(this).html();
 
-          if(content.length > showChar) {
+           if(content.length > showChar) {
 
-              var c = content.substr(0, showChar);
-              var h = content.substr(showChar, content.length - showChar);
+               var truncStr = content.substr(0, showChar);
+               var remainderStr = content.substr(showChar, content.length - showChar);
 
-              var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+               var showOrNot = truncStr + '<span class="ellipses">' + ellipsestext + '&nbsp;</span><span class="morecontent"><span>' + remainderStr + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
 
-              $(this).html(html);
-          }
-      });
+               $(this).html(showOrNot);
+           }
 
-      $(".morelink").click(function(){
-          if($(this).hasClass("less")) {
-              $(this).removeClass("less");
-              $(this).html(moretext);
-          } else {
-              $(this).addClass("less");
-              $(this).html(lesstext);
-          }
-          $(this).parent().prev().toggle();
-          $(this).prev().toggle();
-          return false;
-      });
+       });
+
+       $(".morelink").click(function(){
+           if($(this).hasClass("less")) {
+               $(this).removeClass("less");
+               $(this).html(moretext);
+           } else {
+               $(this).addClass("less");
+               $(this).html(lesstext);
+           }
+           $(this).parent().prev().toggle();
+           $(this).prev().toggle();
+           return false;
+       });
 
     	return result;
+
   };  // end showInfo
+
 
   // determine number of results to be appended to DOM element
   var NumSearchResults = function(query, resultNum) {
